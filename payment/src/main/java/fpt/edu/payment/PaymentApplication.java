6c8798360057d0736ed452f986d7a69701d33c17
robert <nhnam13@cmcglobal.vn>
2022-04-22 09:46:33 +0700

    base project

---
+++ payment/src/main/java/fpt/edu/payment/PaymentApplication.java
@@ -0,0 +1,13 @@
+package fpt.edu.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+
+}
